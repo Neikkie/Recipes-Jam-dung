5e9675c03ed26652110e0d10185d6c341b73dfd8
Neikkie <neikkie21@outlook.com>
2026-05-03 06:36:45 -0400

    Initial Commit

---
+++ Recipes Jam-dung/Recipes_Jam_dungApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_Jam_dungApp.swift
+//  Recipes Jam-dung
+//
+//  Created by Shanique Beckford on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_Jam_dungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
